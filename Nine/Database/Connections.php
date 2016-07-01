84ad6b968515e4837aa52e62ba3bed8dbe5a5d84
Greg Truesdell <odd.greg@gmail.com>
2016-06-30 22:53:20 -0700

    add access methods

--- Nine/Database/Connections.php
+++ Nine/Database/Connections.php
@@ -1,8 +1,9 @@
 <?php namespace Nine\Database;
 
 use Aura\Sql\ExtendedPdo;
-use Illuminate\Database\Connectors\ConnectionFactory;
 use Nine\Exceptions\DBConnectionNotFound;
+use Nine\Library\Arrays;
+use PDO;
 
 /**
  * @package Nine
@@ -17,48 +18,130 @@ class Connections
     /** @var array */
     protected $config;
 
-    /** @var ConnectionFactory */
-    protected $factory;
-
-    public function __construct(ConnectionFactory $factory, array $config = [])
+    public function __construct(array $config = [])
     {
-        // $connections must be an array of the form:
-        //  [
-        //      'default' => [
-        //          'driver'    => 'mysql', ...
-        //      ],
-        //      etc.
-        //  ]
-        // ie: $connections = new Connections(config('database.connections')
+        // connections configuration
         $this->config = $config;
-        $this->factory = $factory;
+        // connection list
+        $this->connections = $config['connections'];
     }
 
     /**
-     * @param string $connection_name
+     * Replaces or inserts alternate values.
      *
-     * @return \PDO
+     * @param array $config
+     *
+     * @return array Returns the resultant config array
+     */
+    public function alterConfig(array $config) : array
+    {
+        return $this->config = array_replace_recursive($this->config, $config);
+    }
+
+    /**
+     *  Closes all open connections and clears the cache.
+     */
+    public function clearCache()
+    {
+        $keys = array_keys($this->cache);
+
+        foreach ($keys as $key) {
+            unset($this->cache[$key]);
+        }
+
+        $this->cache = [];
+    }
+
+    /**
+     * Get a configuration attribute or return $default value if not found.
+     *
+     * @param string     $key
+     * @param null|mixed $default
+     *
+     * @return null
+     */
+    public function getConfig(string $key, $default = NULL)
+    {
+        return Arrays::array_query($this->config, $key, $default);
+    }
+
+    public function getCache()
+    {
+        //return (new \ArrayObject($this->cache))->getArrayCopy(); # ::getArrayCopy($this->cache);
+        return $this->cache;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return PDO
      * @throws DBConnectionNotFound
      */
-    public function getConnection(string $connection_name = 'default') : \PDO
+    public function getConnection(string $name = 'default') : PDO
     {
         // fail if the connection name cannot be found
-        if ( ! isset($this->config[$connection_name]) and ! isset($this->cache[$connection_name])) {
-            throw new DBConnectionNotFound("Connection name `$connection_name` not found.");
+        if ( ! isset($this->connections[$name]) and ! isset($this->cache[$name])) {
+            throw new DBConnectionNotFound("Connection name `$name` not found.");
         }
 
         // return the active connection if it is in the cache
-        if (isset($this->cache[$connection_name])) {
-            return $this->cache[$connection_name];
+        if (isset($this->cache[$name])) {
+            return $this->cache[$name];
         }
 
-        // connect and cache the connection
-        $this->cache[$connection_name] =
-            new ExtendedPdo($this->factory->createConnector($this->config[$connection_name])
-                                          ->connect($this->config[$connection_name]));
+        $connection = $this->connections[$name];
 
-        // return the active connection
-        return $this->cache[$connection_name];
+        return $this->cache[$name] = $this->makeDriverConnection($connection['driver'], $connection);
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function isCached(string $name) : bool
+    {
+        return isset($this->connections[$name]);
+    }
+
+    public function setConfig(array $config)
+    {
+        $this->config = [];
+        $this->config = array_merge($this->config, $config);
+    }
+
+    /**
+     * @param string $driver
+     * @param array  $connection
+     *
+     * @return ExtendedPdo|PDO
+     */
+    private function makeDriverConnection(string $driver, array $connection)
+    {
+        if ($driver === 'sqlite') {
+
+            $dsn = "{$connection['driver']}:{$connection['database']}";
+            $PDO = new ExtendedPdo($dsn, [], [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            ]);
+
+            return new ExtendedPdo($PDO);
+        }
+
+        $dsn = "{$driver}:host={$connection['host']};dbname={$connection['database']}";
+        $PDO = new PDO(
+            $dsn,
+            $connection['username'],
+            $connection['password']
+        );
+
+        $PDO->setAttribute(PDO::ATTR_EMULATE_PREPARES, TRUE);
+        $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $PDO->setAttribute(PDO::ATTR_PERSISTENT, $this->config['persistent']);
+        $PDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, $this->config['fetch']);
+
+        # extend using Aura\SQL PDO extension
+        return new ExtendedPdo($PDO);
 
     }
 
