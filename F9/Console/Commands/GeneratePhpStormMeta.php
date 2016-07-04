9abec628177540667591088e2b7e12a995460d41
Greg Truesdell <odd.greg@gmail.com>
2016-07-04 08:59:41 -0700

    create the `generate:phpstorm_meta` command

--- F9/Console/Commands/GeneratePhpStormMeta.php
+++ F9/Console/Commands/GeneratePhpStormMeta.php
@@ -1,6 +1,20 @@
 <?php namespace Console\Commands;
 
+use F9\Application\Application;
+use Nine\Collections\Attributes;
+use Nine\Collections\Config;
+use Nine\Collections\GlobalScope;
+use Nine\Collections\Paths;
 use Nine\Containers\Forge;
+use Nine\Database\Connections;
+use Nine\Database\Database;
+use Nine\Database\DB;
+use Nine\Database\NineBase;
+use Nine\Views\Blade;
+use Nine\Views\BladeView;
+use Nine\Views\BladeViewConfigurationInterface;
+use Nine\Views\TwigView;
+use Nine\Views\TwigViewConfigurationInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Console\Input\InputInterface;
@@ -29,6 +43,26 @@ class GeneratePhpStormMeta extends Command
     <info>formula generate:phpstorm_meta</info>
 EOT
             );
+
+        //@formatter:off
+        $application    = forge()->make(Application::class);
+        $attributes     = forge()->make(Attributes::class);
+        $blade          = forge()->make(Blade::class);
+        $blade_config   = forge()->make(BladeViewConfigurationInterface::class);
+        $blade_view     = forge()->make(BladeView::class);
+        $config         = forge()->make(Config::class);
+        $connections    = forge()->make(Connections::class);
+        $database       = forge()->make(Database::class);
+        $db             = forge()->make(DB::class);
+        $global_scope   = forge()->make(GlobalScope::class);
+        $ninebase       = forge()->make(NineBase::class);
+        $paths          = forge()->make(Paths::class);
+        $twig_config    = forge()->make(TwigViewConfigurationInterface::class);
+        $twig_view      = forge()->make(TwigView::class);
+
+        $db_connection  = app('db.connection');
+        $db_manager     = app('db');
+
     }
 
     /**
