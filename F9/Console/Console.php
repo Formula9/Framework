ae850a62e9e148db38035a63acd219f12b0ddcdd
Greg Truesdell <odd.greg@gmail.com>
2016-07-04 19:50:51 -0700

    coverted Console to include cloned artisan migration commands.

--- F9/Console/Console.php
+++ F9/Console/Console.php
@@ -1,10 +1,11 @@
 <?php namespace F9\Console;
 
+use F9\Providers\MigrationServiceProvider;
+use Illuminate\Console\Application;
 use Illuminate\Filesystem\ClassFinder;
 use Nine\Collections\ConfigInterface;
 use Nine\Collections\Paths;
 use Nine\Library\Lib;
-use Symfony\Component\Console\Application;
 
 /**
  * @package Nine
@@ -30,16 +31,21 @@ class Console extends Application
      *
      * @param ConfigInterface $config
      * @param Paths           $paths
+     * @param string          $version
      */
-    public function __construct(ConfigInterface $config, Paths $paths)
+    public function __construct(ConfigInterface $config, Paths $paths, $version = \F9\Application\Application::VERSION)
     {
         $this->config = $config;
         $this->paths = $paths;
 
-        parent::__construct('Formula Nine', \F9\Application\Application::VERSION);
+        parent::__construct(forge('illuminate.container'), forge('illuminate.events'), $version);
+
+        (new MigrationServiceProvider(forge('app')))->register(forge('app'));
 
         // in all cases, register the framework commands
         $this->registerFrameworkCommands();
+
+        $this->registerArtisanCommands();
     }
 
     /**
@@ -86,6 +92,20 @@ class Console extends Application
         // this may not be the only call to register application mode commands,
         // so merge with the app commands array.
         $this->app_commands = array_merge($this->app_commands, $commands);
+    }
+
+    /**
+     *  Register the Artisan migration commands.
+     */
+    private function registerArtisanCommands()
+    {
+        $this->add(app('command.migrate'));
+        $this->add(app('command.migrate.install'));
+        $this->add(app('command.migrate.make'));
+        $this->add(app('command.migrate.refresh'));
+        $this->add(app('command.migrate.reset'));
+        $this->add(app('command.migrate.rollback'));
+        $this->add(app('command.migrate.status'));
     }
 
     /**
